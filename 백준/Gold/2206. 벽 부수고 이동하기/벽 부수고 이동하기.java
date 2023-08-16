14274134196eaf04be647b5407b93292f8182b88
김민지 <66842566+Flre-fly@users.noreply.github.com>
2023-08-16 16:19:45 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 780 ms, Memory: 163584 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    static int miro[][];
+    static boolean visited[][][];
+    static int dx[] = {0,0,1,-1};
+    static int dy[] = {1,-1,0,0};
+    static class Point{
+        int r;
+        int c;
+        int k;
+        int t;
+        Point(int r, int c, int k, int t){
+            this.r=r;
+            this.c=c;
+            this.k=k;
+            this.t=t;
+        }
+    }
+    static int bfs(){
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0,0,0,1));
+        int min = Integer.MAX_VALUE;
+        while(!q.isEmpty()){
+            Point cur = q.poll();
+            visited[cur.r][cur.c][cur.k] = true;
+            if(cur.r==r-1 && cur.c==c-1) min = Math.min(cur.t, min);
+            for(int i=0;i<4;i++){
+                int rr = cur.r+dx[i];
+                int cc = cur.c+dy[i];
+                if(rr>=r|| cc>=c|| rr<0|| cc<0|| visited[rr][cc][cur.k]) continue;
+                if(rr==r-1 && cc==c-1) min = Math.min(cur.t+1, min);
+                if(miro[rr][cc]==0){
+                    visited[rr][cc][cur.k] = true;
+                    q.add(new Point(rr,cc,cur.k, cur.t+1));
+                }else{
+                    if(cur.k==0){//이 경우에만 최소 한번만 갈수있음
+                        visited[rr][cc][cur.k+1] = true;
+                        q.add(new Point(rr,cc,cur.k+1, cur.t+1));
+                    }
+                }
+            }
+        }
+        return min==Integer.MAX_VALUE?-1:min;
+    }
+    static int r;
+    static int c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = br.readLine().split(" ");
+        r = Integer.parseInt(input[0]);
+        c = Integer.parseInt(input[1]);
+        visited = new boolean[r][c][2];
+        miro = new int[r][c];
+        for(int i=0;i<r;i++){
+            char arr[] = br.readLine().toCharArray();
+            for(int j=0;j<c;j++){
+                miro[i][j] = arr[j]-'0';
+            }
+        }
+        System.out.print(bfs());
+    }
+
+}
